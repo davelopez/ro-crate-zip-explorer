ab13259ab0ac47e758c8089757aa767540a3f8ef
davelopez <46503462+davelopez@users.noreply.github.com>
2024-09-25 14:47:28 +0200

    Refactor CSS and implement BrowseLocal component

--- examples/vue/ro-crate-zip-vue/src/components/BrowseLocal.vue
+++ examples/vue/ro-crate-zip-vue/src/components/BrowseLocal.vue
@@ -1,9 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, reactive, computed } from "vue";
+import { useFileSystemAccess } from "@vueuse/core";
+
+const dataType = ref<"Text" | "ArrayBuffer" | "Blob">("ArrayBuffer");
+const fileSystemAccess = useFileSystemAccess({
+  dataType,
+  types: [
+    {
+      description: "Zip files",
+      accept: {
+        "application/zip": [".zip"],
+      },
+    },
+  ],
+  excludeAcceptAllOption: true,
+});
+
+const browserSupported = computed(() => results.isSupported);
+
+const results = reactive({
+  isSupported: fileSystemAccess.isSupported,
+  fileName: fileSystemAccess.fileName,
+  fileMIME: fileSystemAccess.fileMIME,
+  fileSize: fileSystemAccess.fileSize,
+  fileLastModified: fileSystemAccess.fileLastModified,
+});
+</script>
 
 <template>
   <main>
     <h2>Browse <span class="green">local</span> RO-Crate Zip files</h2>
 
-    TODO: Implement the BrowseLocal component.
+    <div v-if="!browserSupported">
+      <p>
+        <span class="red"
+          >Your browser does not support the
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/API/File_System_API"
+            target="_blank"
+            rel="noopener noreferrer"
+            >File System Access API</a
+          >.</span
+        >
+        <br />
+        Please use a
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/API/Window/showOpenFilePicker#browser_compatibility"
+          target="_blank"
+          rel="noopener noreferrer"
+          >browser that supports this API</a
+        >, such as the latest version of Chrome, Edge, or Opera.
+      </p>
+    </div>
+
+    <button :disabled="!browserSupported" @click="fileSystemAccess.open()">Browse</button>
   </main>
 </template>
