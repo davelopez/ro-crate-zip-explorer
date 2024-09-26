30de08fdf8ba38b8775214171b13ce1e1a0432df
davelopez <46503462+davelopez@users.noreply.github.com>
2024-09-26 16:05:08 +0200

    Update BrowseLocal component example to use ROCrateZipExplorer

--- examples/vue/ro-crate-zip-vue/src/components/BrowseLocal.vue
+++ examples/vue/ro-crate-zip-vue/src/components/BrowseLocal.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref, reactive, computed } from "vue";
+import { ref, reactive, computed, watch } from "vue";
 import { useFileSystemAccess } from "@vueuse/core";
+import { ROCrateZipExplorer, type ROCrateZip } from "ro-crate-zip-explorer";
 
 const dataType = ref<"Text" | "ArrayBuffer" | "Blob">("ArrayBuffer");
 const fileSystemAccess = useFileSystemAccess({
@@ -24,6 +25,16 @@ const results = reactive({
   fileMIME: fileSystemAccess.fileMIME,
   fileSize: fileSystemAccess.fileSize,
   fileLastModified: fileSystemAccess.fileLastModified,
+  file: fileSystemAccess.file,
+});
+
+const roCrateZipFile = ref<ROCrateZip>();
+
+watch(results, async () => {
+  if (results.file) {
+    const explorer = new ROCrateZipExplorer(results.file);
+    roCrateZipFile.value = await explorer.open();
+  }
 });
 </script>
 
@@ -54,5 +65,26 @@ const results = reactive({
     </div>
 
     <button :disabled="!browserSupported" @click="fileSystemAccess.open()">Browse</button>
+
+    <div v-if="results.fileName">
+      <p>
+        <span class="green">File:</span> {{ results.fileName }}
+        <br />
+        <span class="green">MIME type:</span> {{ results.fileMIME }}
+        <br />
+        <span class="green">Size:</span> {{ results.fileSize }} bytes
+        <br />
+        <span class="green">Last modified:</span> {{ new Date(results.fileLastModified) }}
+      </p>
+
+      <div v-if="roCrateZipFile">
+        <h3>RO-Crate Zip file contents</h3>
+        <ul>
+          <li v-for="file in roCrateZipFile.files" :key="file.filename">
+            {{ file.filename }}
+          </li>
+        </ul>
+      </div>
+    </div>
   </main>
 </template>
