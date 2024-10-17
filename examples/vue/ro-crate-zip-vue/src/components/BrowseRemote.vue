2f4cf00b7d982e9b32632368c21d6d760fe2e698
davelopez <46503462+davelopez@users.noreply.github.com>
2024-10-17 09:56:59 +0200

    Add remote RO-Crate Zip file exploration component

--- examples/vue/ro-crate-zip-vue/src/components/BrowseRemote.vue
+++ examples/vue/ro-crate-zip-vue/src/components/BrowseRemote.vue
@@ -1,4 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import { ROCrateZipExplorer, type ROCrateZip } from "ro-crate-zip-explorer";
+import Explorer from "./Explorer.vue";
+
+const url = ref<string>(
+  "https://raw.githubusercontent.com/davelopez/ro-crate-zip-explorer/refs/heads/main/tests/test-data/simple-invocation.rocrate.zip",
+);
+const roCrateZipFile = ref<ROCrateZip>();
+
+const canExploreRemote = computed(() => {
+  try {
+    new URL(url.value);
+    return true;
+  } catch {
+    return false;
+  }
+});
+
+const exploreTooltip = computed(() => {
+  if (canExploreRemote.value) {
+    return "";
+  } else {
+    return "Please enter a valid URL";
+  }
+});
+
+async function exploreRemote() {
+  console.log("URL", url.value);
+  const explorer = new ROCrateZipExplorer(url.value);
+  roCrateZipFile.value = await explorer.open();
+}
+</script>
 
 <template>
   <main>
@@ -13,6 +45,19 @@
       >.
     </p>
 
-    TODO: Implement the BrowseRemote component.
+    <input type="url" v-model="url" placeholder="Enter the URL of the RO-Crate Zip file" required class="url-input" />
+
+    <button :disabled="!canExploreRemote" @click="exploreRemote" class="button" :title="exploreTooltip">Explore</button>
+
+    <Explorer :ro-crate-zip-file="roCrateZipFile" />
   </main>
 </template>
+
+<style scoped>
+.url-input {
+  width: 90%;
+  padding: 0.5rem;
+  margin: 1rem 0;
+  font-size: 1rem;
+}
+</style>
