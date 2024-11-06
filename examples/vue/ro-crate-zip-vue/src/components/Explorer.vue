27499db7601f353f81a4794cf3f06107dc3c8c62
davelopez <46503462+davelopez@users.noreply.github.com>
2024-11-06 19:11:40 +0100

    Add download functionality for zip file entries in Explorer.vue

--- examples/vue/ro-crate-zip-vue/src/components/Explorer.vue
+++ examples/vue/ro-crate-zip-vue/src/components/Explorer.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
-import { type ROCrateZip } from "ro-crate-zip-explorer";
+import type { ROCrateZip, ZipFileEntry } from "ro-crate-zip-explorer";
 
 interface Props {
   roCrateZipFile?: ROCrateZip;
 }
 
 const props = defineProps<Props>();
+
+async function downloadFile(entry: ZipFileEntry) {
+  const fileData = await entry.data();
+  const blob = new Blob([fileData], { type: "application/octet-stream" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = entry.path;
+  a.click();
+  URL.revokeObjectURL(url);
+}
 </script>
 
 <template>
   <div v-if="roCrateZipFile">
     <h3>RO-Crate Zip file contents</h3>
     <ul>
-      <li v-for="file in roCrateZipFile.files" :key="file.filename">
-        {{ file.filename }}
+      <li v-for="file in roCrateZipFile.zipEntries" :key="file.path">
+        {{ file.path }} <a v-if="file.type === 'File'" @click="downloadFile(file as ZipFileEntry)">Download</a>
       </li>
     </ul>
   </div>
