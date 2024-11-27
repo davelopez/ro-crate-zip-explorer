7e9df5a73a9690ccd3c42e3a9d1639111546fa1d
davelopez <46503462+davelopez@users.noreply.github.com>
2024-11-27 10:27:07 +0100

    Refactor test utilities to provide structured test ZIP file services and expectations

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -1,21 +1,26 @@
 import fs from "fs";
 import path from "path";
 import util from "util";
+import type { ZipService } from "../src/interfaces";
+import { LocalZipService, RemoteZipService } from "../src/zip";
 
 const readFile = util.promisify(fs.readFile);
 
 type TestFileNames = "simple-invocation.rocrate.zip" | "zip64-test.zip";
 
+/**
+ * Convenience methods for retrieving test ZIP files and their expected results.
+ */
 interface TestFileProvider {
   /**
-   * Returns a File object containing the contents of the specified test ZIP file from the test-data directory.
+   * Returns a local test ZIP file service and its expected results from the test-data directory.
    */
-  local(testFileName: TestFileNames): Promise<File>;
+  local(testFileName: TestFileNames): Promise<TestZipFile>;
 
   /**
-   * Returns the URL of the specified test ZIP file from the test-data directory on GitHub.
+   * Returns a remote test ZIP file service and its expected results from the test-data directory on GitHub.
    */
-  remote(testFileName: TestFileNames): Promise<string>;
+  remote(testFileName: TestFileNames): Promise<TestZipFile>;
 }
 
 export const testFileProvider: TestFileProvider = {
@@ -23,12 +28,58 @@ export const testFileProvider: TestFileProvider = {
   remote: getTestZipUrl,
 };
 
-async function getTestZipFile(testFileName: TestFileNames): Promise<File> {
+async function getTestZipFile(testFileName: TestFileNames): Promise<TestZipFile> {
   const zipContents = await readFile(path.resolve(__dirname, "..", "tests", "test-data", testFileName));
   const file = new File([zipContents], testFileName);
-  return file;
+  return {
+    zipService: new LocalZipService(file),
+    expectations: {
+      entriesCount: 19,
+      zipSize: zipContents.length,
+      isZip64: testFileName === "zip64-test.zip",
+    },
+  };
 }
 
-async function getTestZipUrl(testFileName: TestFileNames): Promise<string> {
-  return Promise.resolve(`https://github.com/davelopez/ro-crate-zip-explorer/raw/main/tests/test-data/${testFileName}`);
+async function getTestZipUrl(testFileName: TestFileNames): Promise<TestZipFile> {
+  const remoteZipUrl = `https://github.com/davelopez/ro-crate-zip-explorer/raw/main/tests/test-data/${testFileName}`;
+  return Promise.resolve({
+    zipService: new RemoteZipService(remoteZipUrl),
+    expectations: {
+      entriesCount: 19,
+      zipSize: 0,
+      isZip64: testFileName === "zip64-test.zip",
+    },
+  });
+}
+
+/**
+ * The expected results of testing a ZIP archive.
+ */
+export interface TestZipExpectations {
+  /**
+   * The expected number of files and directories in the ZIP archive.
+   */
+  entriesCount: number;
+
+  /**
+   * The expected size of the ZIP archive.
+   */
+  zipSize: number;
+
+  /**
+   * Whether the ZIP archive is a ZIP64 file.
+   */
+  isZip64: boolean;
+}
+
+/**
+ * A test ZIP service and its expected results.
+ */
+export interface TestZipFile {
+  /** The ZIP service to test. */
+  zipService: ZipService;
+
+  /** The expected results of testing the ZIP service. */
+  expectations: TestZipExpectations;
 }
--- tests/zipService.test.ts
+++ tests/zipService.test.ts
@@ -1,88 +1,65 @@
 import { beforeAll, describe, expect, it } from "vitest";
-import type { ZipArchive, ZipService } from "../src/interfaces";
-import { LocalZipService, RemoteZipService } from "../src/zip";
-import { testFileProvider } from "./testUtils";
+import type { ZipArchive } from "../src/interfaces";
+import { testFileProvider, type TestZipFile } from "./testUtils";
 
 describe("LocalZipService Implementation", async () => {
-  const file = await testFileProvider.local("simple-invocation.rocrate.zip");
+  const testFile = await testFileProvider.local("simple-invocation.rocrate.zip");
 
-  testZipService(() => new LocalZipService(file));
+  testZipService(testFile);
 });
 
 describe("RemoteZipService Implementation", async () => {
-  const url = await testFileProvider.remote("simple-invocation.rocrate.zip");
+  const testFile = await testFileProvider.remote("simple-invocation.rocrate.zip");
 
-  testZipService(() => new RemoteZipService(url));
+  testZipService(testFile);
 });
 
-describe("ZIP64 Support", () => {
-  describe("zipArchive.isZip64", () => {
-    it("should return false given a regular zip", async () => {
-      const file = await testFileProvider.local("simple-invocation.rocrate.zip");
-
-      const service = new LocalZipService(file);
-      const zipArchive = await service.open();
-      expect(zipArchive.isZip64).toBe(false);
-    });
-
-    it("should return true given a ZIP64 file", async () => {
-      const file = await testFileProvider.local("zip64-test.zip");
-
-      const service = new LocalZipService(file);
-      const zipArchive = await service.open();
-      expect(zipArchive.isZip64).toBe(true);
-    });
-
-    it("should return true given a ZIP64 file from a remote URL", async () => {
-      const url = await testFileProvider.remote("zip64-test.zip");
-
-      const service = new RemoteZipService(url);
-      const zipArchive = await service.open();
-      expect(zipArchive.isZip64).toBe(true);
-    });
-  });
-});
-
-const testZipService = (createZipService: () => ZipService) => {
-  let zipService: ZipService;
+const testZipService = (zipTestFile: TestZipFile) => {
+  const zipService = zipTestFile.zipService;
+  const expectations = zipTestFile.expectations;
   let zipArchive: ZipArchive;
 
   beforeAll(async () => {
-    zipService = createZipService();
-    zipArchive = await zipService.open();
+    zipArchive = await zipTestFile.zipService.open();
   });
 
-  describe("listFiles", () => {
-    it("should return a list with all the files and directories contained in the remote Zip archive", () => {
-      expect(zipArchive.entries.length).toBeGreaterThan(0);
-    });
-  });
-
-  describe("extractFile", () => {
-    it("should decompress and return the content of the file in the remote Zip archive", async () => {
-      expect(zipArchive.entries.length).toBeGreaterThan(0);
-      const remoteMetadataFile = zipArchive.findFileByName("ro-crate-metadata.json");
-
-      if (!remoteMetadataFile) {
-        throw new Error("No RO-Crate metadata file found in the ZIP archive");
-      }
-
-      const metadataFileData = await zipService.extractFile(remoteMetadataFile);
-      expect(metadataFileData).toBeDefined();
-
-      const metadataFileText = new TextDecoder().decode(metadataFileData);
-      const metadataObject = JSON.parse(metadataFileText) as Record<string, unknown>;
-      verifyMetadataContext(metadataObject);
+  describe("zipArchive", () => {
+    describe("listFiles", () => {
+      it("should return a list with all the files and directories contained in the remote Zip archive", () => {
+        expect(zipArchive.entries.length).toBe(expectations.entriesCount);
+      });
     });
 
-    function verifyMetadataContext(json: Record<string, unknown>) {
-      const expectedContextUrl = "https://w3id.org/ro/crate/1.1/context";
-      const context = json["@context"];
-      if (typeof context === "string") {
-        expect(context).toBe(expectedContextUrl);
-      } else if (Array.isArray(context)) {
-        expect(context[0]).toBe(expectedContextUrl);
+    describe("extractFile", () => {
+      it("should decompress and return the content of the file in the remote Zip archive", async () => {
+        expect(zipArchive.entries.length).toBeGreaterThan(0);
+        const remoteMetadataFile = zipArchive.findFileByName("ro-crate-metadata.json");
+
+        if (!remoteMetadataFile) {
+          throw new Error("No RO-Crate metadata file found in the ZIP archive");
+        }
+
+        const metadataFileData = await zipService.extractFile(remoteMetadataFile);
+        expect(metadataFileData).toBeDefined();
+
+        const metadataFileText = new TextDecoder().decode(metadataFileData);
+        const metadataObject = JSON.parse(metadataFileText) as Record<string, unknown>;
+        verifyMetadataContext(metadataObject);
+      });
+
+      function verifyMetadataContext(json: Record<string, unknown>) {
+        const expectedContextUrl = "https://w3id.org/ro/crate/1.1/context";
+        const context = json["@context"];
+        if (typeof context === "string") {
+          expect(context).toBe(expectedContextUrl);
+        } else if (Array.isArray(context)) {
+          expect(context[0]).toBe(expectedContextUrl);
+        }
       }
-    }
+    });
+
+    it("should return the expected value for isZip64", () => {
+      expect(zipArchive.isZip64).toBe(expectations.isZip64);
+    });
   });
 };
