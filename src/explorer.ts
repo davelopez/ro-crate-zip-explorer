425bb1acbcc65cc3272a4019d9e25a71513ba496
davelopez <46503462+davelopez@users.noreply.github.com>
2024-11-27 12:22:06 +0100

    Refactor ROCrateZipExplorer

--- src/explorer.ts
+++ src/explorer.ts
@@ -1,5 +1,5 @@
 import { ROCrate } from "ro-crate";
-import type { ROCrateZip, ZipFileEntry, ZipService } from "./interfaces.js";
+import type { ROCrateZip, ZipArchive, ZipFileEntry, ZipService } from "./interfaces.js";
 import { LocalZipService, RemoteZipService } from "./zip";
 export type { ROCrateZip };
 
@@ -20,24 +20,32 @@ const ROCRATE_METADATA_FILENAME = "ro-crate-metadata.json";
  */
 export class ROCrateZipExplorer {
   private zipService: ZipService;
-  private crateZip?: ROCrateZip;
+  private zipArchive?: ZipArchive;
+  private crate?: ROCrate;
 
   public constructor(source: File | string) {
     this.zipService = zipServiceFactory(source);
+  }
+
+  private get crateZip(): ROCrateZip | undefined {
+    if (!this.crate || !this.zipArchive) {
+      return undefined;
+    }
+    return { crate: this.crate, zip: this.zipArchive };
   }
 
   public async open(): Promise<ROCrateZip> {
     if (this.crateZip) {
       return this.crateZip;
     }
-    const zip = await this.zipService.open();
-    const crate = await this.extractROCrateMetadata();
-    this.crateZip = { crate, zip };
-    return this.crateZip;
+    this.zipArchive = await this.zipService.open();
+    this.crate = await this.extractROCrateMetadata();
+    const crateZip = { crate: this.crate, zip: this.zipArchive };
+    return crateZip;
   }
 
   public async getFileContents(fileEntry: ZipFileEntry) {
-    await this.zipService.extractFile(fileEntry);
+    return await this.zipService.extractFile(fileEntry);
   }
 
   private async extractROCrateMetadata(): Promise<ROCrate> {
@@ -50,10 +58,10 @@ export class ROCrateZipExplorer {
   }
 
   private findCrateEntry(): ZipFileEntry {
-    if (!this.crateZip) {
+    if (!this.zipArchive) {
       throw new Error("The ZIP archive has not been opened yet. Call the `open` method first.");
     }
-    const roCrateFileEntry = this.crateZip.zip.findFileByName(ROCRATE_METADATA_FILENAME);
+    const roCrateFileEntry = this.zipArchive.findFileByName(ROCRATE_METADATA_FILENAME);
     if (!roCrateFileEntry) {
       throw new Error("No RO-Crate metadata file found in the ZIP archive");
     }
