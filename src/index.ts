a3e7c7cf03c0eaaa1df485f5fa0384b89daaa1a5
davelopez <46503462+davelopez@users.noreply.github.com>
2025-03-07 11:35:48 +0100

    Improve exposure of ro-crate types
    This will allow consumers of the library to access the types related to the RO-Crate class.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { ROCrateZipExplorer, type ROCrateZip } from "./explorer.js";
 export type { AnyZipEntry, ZipDirectoryEntry, ZipFileEntry } from "./interfaces.js";
+export type { Entity as ROCrateEntity, ROCrateReadOnlyView } from "./types/ro-crate-interfaces.js";
