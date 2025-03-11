cecfc93520197f322f0baea8a7bf23a8cee0bf0d
davelopez <46503462+davelopez@users.noreply.github.com>
2025-03-11 17:12:22 +0100

    Expose ZipArchive type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { ROCrateZipExplorer, ZipExplorer } from "./explorer.js";
-export type { AnyZipEntry, ZipDirectoryEntry, ZipFileEntry } from "./interfaces.js";
+export type { AnyZipEntry, ZipArchive, ZipDirectoryEntry, ZipFileEntry } from "./interfaces.js";
 export type { Entity as ROCrateEntity, ROCrateImmutableView } from "./types/ro-crate-interfaces.js";
