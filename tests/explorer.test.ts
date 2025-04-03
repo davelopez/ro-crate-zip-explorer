f1eb102917f2d0838d6a077dd3b9ee4e63ad23db
davelopez <46503462+davelopez@users.noreply.github.com>
2025-04-03 12:43:08 +0200

    Increase test coverage

--- tests/explorer.test.ts
+++ tests/explorer.test.ts
@@ -32,6 +32,15 @@ describe("ROCrateZipExplorer", () => {
     testROCrateExplorer(explorer);
   });
 
+  describe("Wrapping a ZipExplorer with ROCrateZipExplorer", async () => {
+    const localTestFile = await testFileProvider.local("rocrate-test.zip");
+    const zipExplorer = new ZipExplorer(localTestFile.source);
+
+    const explorer = new ROCrateZipExplorer(zipExplorer);
+
+    testROCrateExplorer(explorer);
+  });
+
   describe("Explore non-RO-Crate ZIP file using ROCrateZipExplorer", async () => {
     const nonROCrateTestFile = await testFileProvider.local("non-rocrate-test.zip");
     const explorer = new ROCrateZipExplorer(nonROCrateTestFile.source);
@@ -64,38 +73,61 @@ describe("ROCrateZipExplorer", () => {
       it("should throw an error when trying to access the RO-Crate metadata", () => {
         expect(() => !!explorer.crate).toThrow("No RO-Crate metadata found in the ZIP archive");
       });
+
+      it("should throw an error when extracting metadata", async () => {
+        await expect(explorer.extractMetadata()).rejects.toThrow("No RO-Crate metadata found in the ZIP archive");
+      });
     });
   });
 });
 
 const testExplorerWithFile = (zipTestFile: TestZipFile) => {
-  let zipArchive: ZipArchive;
   let explorer: IZipExplorer;
 
   beforeAll(async () => {
     explorer = new ZipExplorer(zipTestFile.source);
-    zipArchive = await explorer.open();
+    await explorer.open();
     await explorer.extractMetadata();
   });
 
   describe("open", () => {
     it("should return the ZIP archive", () => {
-      expect(zipArchive).toBeDefined();
+      expect(explorer.zipArchive).toBeDefined();
     });
 
     it("should have the correct number of entries in the ZIP archive", () => {
-      expect(zipArchive.entries.size).toBe(zipTestFile.expectations.entriesCount);
+      expect(explorer.zipArchive.entries.size).toBe(zipTestFile.expectations.entriesCount);
     });
 
     it("should return the same ZIP archive when called again", async () => {
       const secondOpen = await explorer.open();
-      expect(secondOpen).toBe(zipArchive);
+      expect(secondOpen).toBe(explorer.zipArchive);
+    });
+  });
+
+  describe("Extract metadata", () => {
+    beforeAll(async () => {
+      await explorer.extractMetadata();
+    });
+
+    it("should extract basic metadata from the ZIP entries", () => {
+      const testFileName = "Trim_on_data_1_1690cb0a3211e932.txt";
+      const testFilePath = `datasets/${testFileName}`;
+      const file = explorer.zipArchive.findFileByName(testFilePath);
+
+      assert(file, `File ${testFilePath} not found in the ZIP archive`);
+
+      const metadata = explorer.getFileEntryMetadata(file);
+      expect(metadata).toBeDefined();
+      expect(metadata.name).toBe(testFileName);
+      expect(metadata.size).toBe(849463);
+      expect(metadata.description).toBeUndefined();
     });
   });
 
   describe("File access", () => {
     it("should extract the contents of a file in the ZIP archive", async () => {
-      const file = zipArchive.findFileByName("ro-crate-metadata.json");
+      const file = explorer.zipArchive.findFileByName("ro-crate-metadata.json");
       assert(file, "File not found in the ZIP archive");
       const fileContents = await explorer.getFileContents(file);
       expect(fileContents).toBeDefined();
@@ -103,53 +135,87 @@ const testExplorerWithFile = (zipTestFile: TestZipFile) => {
     });
   });
 
-  describe("Find files", () => {
+  describe("Find files by name", () => {
     it("should find a file by its name in the ZIP archive", () => {
-      const file = zipArchive.findFileByName("ro-crate-metadata.json");
+      const file = explorer.zipArchive.findFileByName("ro-crate-metadata.json");
       assert(file, "File not found in the ZIP archive");
       expect(file.path).toBe("ro-crate-metadata.json");
     });
 
     it("should return undefined if the file is not found", () => {
-      const file = zipArchive.findFileByName("nonexistent.txt");
+      const file = explorer.zipArchive.findFileByName("nonexistent.txt");
       expect(file).toBeUndefined();
     });
   });
 };
 
 const testROCrateExplorer = (explorer: ROCrateZipExplorer) => {
-  let zipArchive: ZipArchive;
-
-  beforeAll(async () => {
-    zipArchive = await explorer.open();
-    await explorer.extractMetadata();
-  });
-
-  describe("crate access", () => {
-    it("should indicate that the ZIP archive contains an RO-Crate", () => {
-      expect(explorer.hasCrate).toBe(true);
-    });
-
-    it("should provide the RO-Crate metadata", () => {
-      expect(explorer.crate).toBeDefined();
-      verifyCrateMetadataContext(explorer.crate);
+  describe("Before opening", () => {
+    it("should throw an error when trying to check the RO-Crate presence", () => {
+      expect(() => explorer.hasCrate).toThrow("Please call open() before accessing the ZIP archive");
     });
   });
 
-  describe("Extract basic file metadata from the crate", () => {
-    it("should extract metadata from a file entry", () => {
-      const entryFileName = "Trim_on_data_1_1690cb0a3211e932.txt";
-      const expectedNameFromMetadata = "Trim on data 1";
+  describe("After opening", () => {
+    const testEntryFileName = "Trim_on_data_1_1690cb0a3211e932.txt";
 
-      const file = zipArchive.findFileByName(entryFileName);
+    beforeAll(async () => {
+      await explorer.open();
+    });
 
-      assert(file, `File ${entryFileName} not found in the ZIP archive`);
+    it("should extract the contents of a file in the ZIP archive", async () => {
+      const file = explorer.zipArchive.findFileByName("ro-crate-metadata.json");
+      assert(file, "File not found in the ZIP archive");
+      const fileContents = await explorer.getFileContents(file);
+      expect(fileContents).toBeDefined();
+      expect(fileContents.byteLength).toBe(10253);
+    });
 
-      const metadata = explorer.getFileEntryMetadata(file);
-      expect(metadata).toBeDefined();
-      expect(metadata.name).toBe(expectedNameFromMetadata);
-      expect(metadata.size).toBe(849463);
-      expect(metadata.description).toBeUndefined();
+    describe("Before extracting metadata", () => {
+      it("should throw an error when trying to access the RO-Crate metadata", () => {
+        expect(() => !!explorer.crate).toThrow("No RO-Crate metadata found in the ZIP archive");
+      });
+
+      it("should throw an error when trying to get file entry metadata", () => {
+        const file = explorer.zipArchive.findFileByName(testEntryFileName);
+        assert(file, "File not found in the ZIP archive");
+        expect(() => explorer.getFileEntryMetadata(file)).toThrow(
+          `Metadata for file ${file.path} not found. Make sure to call extractMetadata() first.`,
+        );
+      });
+    });
+
+    describe("After extracting metadata", () => {
+      beforeAll(async () => {
+        await explorer.extractMetadata();
+      });
+
+      describe("crate access", () => {
+        it("should indicate that the ZIP archive contains an RO-Crate", () => {
+          expect(explorer.hasCrate).toBe(true);
+        });
+
+        it("should provide the RO-Crate metadata", () => {
+          expect(explorer.crate).toBeDefined();
+          verifyCrateMetadataContext(explorer.crate);
+        });
+      });
+
+      describe("Extract basic file metadata from the crate", () => {
+        it("should extract metadata from a file entry", () => {
+          const expectedNameFromMetadata = "Trim on data 1";
+
+          const file = explorer.zipArchive.findFileByName(testEntryFileName);
+
+          assert(file, `File ${testEntryFileName} not found in the ZIP archive`);
+
+          const metadata = explorer.getFileEntryMetadata(file);
+          expect(metadata).toBeDefined();
+          expect(metadata.name).toBe(expectedNameFromMetadata);
+          expect(metadata.size).toBe(849463);
+          expect(metadata.description).toBeUndefined();
+        });
+      });
     });
   });
 };
