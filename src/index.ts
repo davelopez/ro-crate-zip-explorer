59e47df1ecaa7b5e2240c12d8f2ecda3fa6a8de7
davelopez <46503462+davelopez@users.noreply.github.com>
2025-04-04 15:00:15 +0200

    Update metadata handling in ZipExplorer and related interfaces
    - Changed FileMetadata to ZipEntryMetadata for better clarity and structure.
    - Updated methods in AbstractFileMetadataProvider to use new ZipEntryMetadata.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 export { isFileEntry, isRemoteZip } from "./utils.js";
 
 export { AbstractZipExplorer, ROCrateZipExplorer, ZipExplorer } from "./explorer.js";
-export type { AnyZipEntry, IZipExplorer, ZipArchive, ZipDirectoryEntry, ZipFileEntry } from "./interfaces.js";
+export type {
+  AnyZipEntry,
+  IZipExplorer,
+  ZipArchive,
+  ZipDirectoryEntry,
+  ZipEntryMetadata,
+  ZipFileEntry,
+} from "./interfaces.js";
 export type { Entity as ROCrateEntity, ROCrateImmutableView } from "./types/ro-crate-interfaces.js";
